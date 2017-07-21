a5456fbb33ed0d2e2684a75c1b41a002a4c6ccec
gabrielthomasjacobs <gjacobs@voyagersearch.com>
2017-07-21 06:07:39 -0700

    VG-5743 - Fixing Create Layer Files Test (#122)

--- test/e2e/tasks/create-layer-files-task-spec.js
+++ test/e2e/tasks/create-layer-files-task-spec.js
@@ -21,11 +21,8 @@ describe('Run Create Layer Files Task', function() {
         var paramList = taskPage.getParams();
 
         // Verify we have the correct number of params and defaults
-        expect(paramList.count()).toBe(2);
+        expect(paramList.count()).toBe(1);
         Util.waitForSpinner();  //can't click until spinner is gone
-
-        // Enter an output file name.
-        setMetaFolderPath();
 
         // Execute the task with default parameter values
         taskPage.executeTask();
@@ -35,13 +32,6 @@ describe('Run Create Layer Files Task', function() {
     afterEach(function() {
         verifyStatus();
     });
-
-    function setMetaFolderPath() {
-        var textInputs = element.all(by.css('[ng-model="param.value"]'));
-        return textInputs.then(function(textInput) {
-            textInput[0].sendKeys("C:\\voyager\\data\\default\\meta");
-        });
-    }
 
     function verifyStatus() {
         // Verify there are no errors or warnings (warnings may be possible bug and to be investigated)
